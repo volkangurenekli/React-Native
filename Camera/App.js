88d74af01e203129d15b9d910589fac55242a2f2
volkangurenekli <tcsgvolkan@gmail.com>
2020-05-08 13:49:43 +0300

    Camera

---
+++ Camera/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import CameraView from './components/CameraView';
+
+export default class App extends Component {
+  render() {
+    return <CameraView />;
+  }
+}
