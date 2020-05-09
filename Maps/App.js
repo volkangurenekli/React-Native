64c9859b943bed6227d3df2a6683c4e7f846c8fb
volkangurenekli <tcsgvolkan@gmail.com>
2020-05-09 23:56:41 +0300

    Maps

---
+++ Maps/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import CurrentPosition from './components/CurrentPosition';
+
+export default class App extends Component {
+  render() {
+    return <CurrentPosition />;
+  }
+}
