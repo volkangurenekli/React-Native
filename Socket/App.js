16ade05c4ed517289bb654324a3987029a58b3ea
volkangurenekli <tcsgvolkan@gmail.com>
2020-05-20 23:19:11 +0300

    real time socket io

--- Socket/App.js
+++ Socket/App.js
@@ -1,114 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import io from 'socket.io-client';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  state = {
+    onlineCount: 0,
+    color: '#f1f1f1',
+  };
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+  componentDidMount() {
+    this.io = io.connect('http://localhost:3000', {
+      timeout: 10000,
+    });
+
+    this.io.on('newUser', onlineCount => {
+      this.setState({
+        onlineCount,
+      });
+    });
+
+    this.io.on('disUser', onlineCount => {
+      this.setState({
+        onlineCount,
+      });
+    });
+
+    this.io.on('changeColor', color => {
+      this.setState({
+        color,
+      });
+    });
+  }
+
+  generateColor = () => {
+    return (
+      '#' +
+      Math.random()
+        .toString(16)
+        .slice(2, 8)
+    );
+  };
+
+  changeColor = () => {
+    let color = this.generateColor();
+
+    this.setState({
+      color,
+    });
+
+    this.io.emit('changeColor', color);
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, {backgroundColor: this.state.color}]}>
+        <TouchableHighlight onPress={this.changeColor} style={styles.button}>
+          <React.Fragment>
+            <Text style={styles.color}>{this.state.color}</Text>
+            <View style={styles.countContainer}>
+              <Text style={styles.count}>{this.state.onlineCount}</Text>
+              <Text>Online now</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </React.Fragment>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  countContainer: {
+    alignItems: 'center',
+    backgroundColor: '#f1f1f1',
+    padding: 15,
+    borderRadius: 5,
   },
-  body: {
-    backgroundColor: Colors.white,
+  count: {
+    fontSize: 32,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  color: {
+    fontSize: 28,
+    padding: 10,
   },
 });
-
-export default App;
